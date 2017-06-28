43090ceaae8399de65c0d29935a69514d600bc91
Sean <sean8purdue@gmail.com>
2017-06-28 16:53:55 -0400

    List restarts when intercepted by a code blocki

--- lab1/lab1src/v1/main.c
+++ lab1/lab1src/v1/main.c
@@ -2,8 +2,14 @@
 // 	and stores the result in z
 //   	or, simply: z = x + y
 
-/*int main()*/
-void main()
+//Problem 1.1
+/*main()*/
+
+// problem 1.2
+/*void main()*/
+
+// original
+int main()
 {
 int x, y, z;
 

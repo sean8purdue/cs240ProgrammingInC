479696b184d2e98640a8e11a7ff316fd18d54ea2
Sean <sean8purdue@gmail.com>
2017-07-09 01:38:28 -0400

    calc_dotmag done

--- lab3/dotmagcode/calc_dotmag.c
+++ lab3/dotmagcode/calc_dotmag.c
@@ -1,5 +1,18 @@
+#include <math.h>
 
-float calc_dotmag(int size, float *x, float *y, float *dp, float *mg) {
+float calc_dotmag(int size, float *x, float *y, float *mgx, float *mgy) {
 
-    return *dp;
+    float dp = 0;
+    float xSum = 0;
+    float ySum = 0;
+    for (int i = 0; i < size; i++) {
+        dp = dp + *(x+i) * *(y+i);
+        xSum += *(x+i) * *(x+i);
+        ySum += *(y+i) * *(y+i);
+    }
+    
+    *mgx = sqrt(xSum);
+    *mgy = sqrt(ySum);
+
+    return dp;
 }
--- lab3/dotmagcode/main.c
+++ lab3/dotmagcode/main.c
@@ -12,8 +12,17 @@ int main() {
     }
 
     // test for read_vectors
-    printf("%d\n", size);
+    /*printf("%d\n", size);*/
     for (int i = 0; i < size; i++) { printf("%f\n", *(x+i)); }
     for (int i = 0; i < size; i++) { printf("%f\n", *(y+i)); }
+
+
+    // calculate dot product and magnitude
+    float dp;
+    float mgx;
+    float mgy;
+
+    dp = calc_dotmag(size, x, y, &mgx, &mgy);
+    printf("%.2f %.2f %.2f\n", dp, mgx, mgy);
 
 }

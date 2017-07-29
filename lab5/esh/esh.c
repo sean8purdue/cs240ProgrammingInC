fe6ff8d1ded291b951bf9977a3bace7bc8f3f8ae
Sean <sean8purdue@gmail.com>
2017-07-29 01:31:13 -0400

    use getCmd function in main

--- lab5/esh/esh.c
+++ lab5/esh/esh.c
@@ -4,6 +4,8 @@
 #define PROMPT 10
 #define CMD 100
 
+void getCmd(char *);
+
 int main() {
     char promt[PROMPT];
     char cmd[CMD];
@@ -11,12 +13,23 @@ int main() {
     strcpy(promt, "$ ");
     /*printf("%s\n", promt);*/
 
-    // get user input string with getchar
-    char c;
-    int i = 0;
-    while ( (c = getchar()) != '\n' && (i < (CMD-1))   ) { cmd[i++] = c; }
-    cmd[i] = '\0';
-    printf("%s\n", cmd);
+    while (1) {
+        printf("%s ", promt);
+
+        // get user input string with getchar
+        getCmd(cmd);
+        printf("%s\n", cmd);
+
+    }
 
     
+}
+
+void getCmd(char *cmd) {
+    char c;
+    int i = 0;
+    while ( (c = getchar()) != '\n' && (i < (CMD-1))   ) {
+        cmd[i++] = c; 
+    }
+    cmd[i] = '\0';
 }
